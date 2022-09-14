89427433f71f3fd677f34886a5b3c06efdd9256f
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-09-14 14:44:42 -0400

    control state with a controlled component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
 
 function ContactForm() {
+
+    //the Hook that'll manage the form data
+const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+      }
+      
+      function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+      }
+
   return(
     <section>
         <h1>Contact Me</h1>
-        <form id="contact-form">
+        <form id="contact-form" onSubmit={handleSubmit}>
           <div>    
             <label htmlFor="name">Name:</label>
-            <input type="text" name="name" />
+            <input type="text" name="name" defaultValue={name} onChange={handleChange} />
           </div>
 
           <div>    
             <label htmlFor="name">Email address:</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" defaultValue={email} onChange={handleChange} />
           </div>
 
           <div>    
             <label htmlFor="name">Message::</label>
-            <textarea type="message" rows="5" />
+            <textarea type="message" rows="5" defaultValue={message} onChange={handleChange} />
           </div>
 
           <button type="submit">submit</button>
